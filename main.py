fb410a66996b1da949d14668a9fd96b6e541a62c
sgcodecampus12 <sgcodecampus12@sgcca.local>
2019-09-01 13:31:23 +0800

    added Layouts

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class MyLayout(GridLayout):
+    pass
+
 
 class WeatherApp(App):
-    pass
+    def build(self):
+        return MyLayout()
 
 if __name__ == "__main__":
     WeatherApp().run()
